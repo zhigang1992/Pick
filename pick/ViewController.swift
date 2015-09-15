cf934b245c2243841c8ac57b2d624f62342fd379
Kyle Fang <zhigang1992@gmail.com>
2015-09-15 18:16:08 +0800

    Refactor

--- pick/ViewController.swift
+++ pick/ViewController.swift
@@ -23,58 +23,35 @@ class ViewController: UIViewController {
 
     let screenRotate:PublishSubject<()> = PublishSubject()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var animating = false
 
-        var animating = false
+    var startSignal:Observable<()> {
+        return tap.rx_event.filter({[unowned self] _ in return !self.animating}).map({ _ in return () })
+            .doOn(next: {[unowned self] _ in self.animating = true})
+    }
 
-        let stopSignal = [tap.rx_event
-            .filter({_ in animating})
-            .map({_ in ()}), disappear]
-            .asObservable()
-            .merge()
-            .doOn(next: {_ in animating = false})
+    var stopSignal:Observable<()> {
+        let manualSignal:Observable<()> = tap.rx_event.filter({[unowned self] _ in return self.animating}).map({ _ in return () })
+        return [manualSignal, self.disappear].asObservable().merge()
+                    .doOn(next: {[unowned self] _ in self.animating = false})
+    }
 
-        let animatingSignal = tap.rx_event
-            .filter({_ in !animating})
-            .doOn(next: {_ in animating = true})
-            .flatMap({ _ in
-                interval(0.03, MainScheduler.sharedInstance)
-                    .takeUntil(stopSignal)
-            })
-            .publish()
-
-        animatingSignal.connect().addDisposableTo(disposeBag)
-
-        animatingSignal.map({ _ -> UIColor in
-            let randomRed = CGFloat(drand48())
-            let randomGreen = CGFloat(drand48())
-            let randomBlue = CGFloat(drand48())
-            return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
-        })
-            .subscribeNext({ [unowned self] color in
-                self.view.backgroundColor = color
-                })
-            .addDisposableTo(disposeBag)
-
-        animatingSignal.map({ _ -> String in
-            return "\(rand() % 100)"
-        })
-            .bindTo(self.text.rx_text)
-            .addDisposableTo(disposeBag)
-
-        [pinch.rx_event
+    var fontSizeSignal:Observable<CGFloat> {
+        let pinchSignal:Observable<CGFloat> = self.pinch.rx_event
             .map({ ($0 as! UIPinchGestureRecognizer).scale })
             .scan((1.0, 1.0)){ ($0.1, $1) }
             .map({ $1 / $0 })
             .filter({ [unowned self] _ in
                 return self.pinch.state == .Changed
-                }),
-            self.screenRotate
-                .map({[unowned self] in
-                    let size = self.view.bounds.size
-                    return size.height / size.width
-                    })].asObservable().merge()
+                })
+
+        let rotateSignal:Observable<CGFloat> = self.screenRotate
+            .map({[unowned self] in
+                let size = self.view.bounds.size
+                return size.height / size.width
+                })
+
+        return  [pinchSignal, rotateSignal].asObservable().merge()
             .map({ [unowned self] scale in
                 return self.text.font.pointSize * scale
                 })
@@ -83,14 +60,38 @@ class ViewController: UIViewController {
                 let fontMaxSize = min(size.width, size.height) * 0.6
                 return max(min(fontSize, fontMaxSize), 40)
             })
+    }
+
+    var holdSignal:Observable<()> {
+        return self.hold.rx_event.filter({$0.state == .Began}).map({_ in return () })
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.startSignal
+            .flatMap({[unowned self] _ -> Observable<Int64> in
+                return interval(0.03, MainScheduler.sharedInstance).takeUntil(self.stopSignal)
+            })
+            .map({ _ -> (UIColor, String) in
+                let color = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+                let text = "\(rand()%100)"
+                return (color, text)
+            })
+            .subscribeNext({[unowned self] color, text in
+                self.text.text = text
+                self.view.backgroundColor = color
+            })
+            .addDisposableTo(disposeBag)
+
+        self.fontSizeSignal
             .map({ UIFont.systemFontOfSize($0) })
             .subscribeNext({ [unowned self] font in
                 self.text.font = font
                 })
             .addDisposableTo(disposeBag)
 
-        hold.rx_event
-            .filter({$0.state == .Began})
+        self.holdSignal
             .subscribeNext({ [unowned self] _ in
                 self.performSegueWithIdentifier(Segue.Settings.identifier!, sender: nil)
                 })
